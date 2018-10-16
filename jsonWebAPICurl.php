4f6971d2994d6cdd7a5f2233850947e78e8cb26b
amarreiros <amarreiros@gmail.com>
2018-10-16 23:08:02 +0100

    generic file

---
+++ jsonWebAPICurl.php
@@ -0,0 +1,42 @@
+<?php	
+	/*
+    * Genric function to make the api calls, using curl
+    * $method : the verb associated with the call of the api (post, put, get, delete)
+    * $url: the base uri of the API
+    * $data: json object with the data to send to the server 
+    */
+    function callJsonAPI($method, $url, $data){
+        $curl = curl_init();
+     
+        switch ($method){
+           case "POST":
+              curl_setopt($curl, CURLOPT_POST, 1);
+              if ($data)
+                 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+              break;
+           case "PUT":
+              curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+              if ($data)
+                 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
+              break;
+           default:
+              if ($data)
+                 $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+     
+        // OPTIONS:
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+        ));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+     
+        // EXECUTE:
+        $result = curl_exec($curl);
+        if(!$result){die("Connection Failure");}
+        curl_close($curl);
+        return $result;
+     }
+     
+?>
